ca6400b51f96d2d86a72e39e71410920dc047630
Etienne <>
2022-09-26 11:33:14 -0400

    set npm auth token

--- src/package-name.ts
+++ src/package-name.ts
@@ -1,4 +1,4 @@
 export const escapePackageName = (packageName: string) => {
   // Same as Definitely Typed: https://github.com/microsoft/TypeScript/issues/14819
-  return `test__${packageName.replace(/^@/, "").replace("/", "__")}`;
+  return packageName.replace(/^@/, "").replace("/", "__");
 };
--- src/publish.ts
+++ src/publish.ts
@@ -7,11 +7,9 @@ const DRY_RUN = process.env.DISABLE_DRY_RUN !== "true";
 export const publishPackage = async (packagePath: string) => {
   const packageJson: PackageJson = require(path.resolve(packagePath, "package.json"));
 
-  console.log("DRY_RUN:", DRY_RUN);
-
   try {
     console.time(`${packageJson.name}@${packageJson.version} has been published`);
-    await exec(`cd "${packagePath}" && npm publish --access public --dry-run`);
+    await exec(`cd "${packagePath}" && npm publish --access public ${DRY_RUN ? "--dry-run" : ""}`);
     console.timeEnd(`${packageJson.name}@${packageJson.version} has been published`);
   } catch (error) {
     if (error instanceof Error && error.message.includes("You cannot publish over the previously published versions")) {
